1da85769e58a841b922209d478eb4e4db1bc53ff
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-07 12:24:59 -0400

    Triangle Area Method

--- ModuleReview1/ModuleReview1/Program.cs
+++ ModuleReview1/ModuleReview1/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         CircleArea(7);
+        TriangleArea(12, 4);
     }
 
     public static void CircleArea(double radius)
@@ -14,5 +15,19 @@ class Program
         double area = Math.PI * rad * rad;
         Console.WriteLine("Area of Circle is:" + area);
     }
+
+    public static void TriangleArea(double baseLength, double height)
+    {
+         
+        Console.WriteLine($"Enter Base Length of Triangle: {baseLength} ");
+        baseLength = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Enter Height of Triangle: {height} ");
+        height = Convert.ToDouble(Console.ReadLine());
+        double area = baseLength * height / 2;
+        Console.WriteLine("Area of Triangle is : {0}", area);
+        Console.ReadLine();
+    }
+
+
 }
 
