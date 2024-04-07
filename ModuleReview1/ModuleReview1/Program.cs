a75caf78b77c8e4119fc8ffbc7c72d51ce0e3446
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-07 11:43:53 -0400

    ModuleReview1

---
+++ ModuleReview1/ModuleReview1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ModuleReview1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CircleArea(7);
+    }
+
+    public static void CircleArea(double radius)
+    {
+        Console.WriteLine($"Enter Radius:{radius} ");
+        double rad = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * rad * rad;
+        Console.WriteLine("Area of Circle is:" + area);
+    }
+}
+
