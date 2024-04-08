f4702b1ebe310a156f769d4b70725cad472ea3b0
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-08 10:28:41 -0400

    Square method

--- ModuleReview1/ModuleReview1/Program.cs
+++ ModuleReview1/ModuleReview1/Program.cs
@@ -13,8 +13,9 @@ class Program
     public static void CircleArea(double radius)
     {
         Console.WriteLine($"Enter Radius:{radius} ");
-        double rad = Convert.ToDouble(Console.ReadLine());
-        double area = Math.PI * rad * rad;
+        double userRad = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("UserRadius is:" + userRad);
+        double area = Math.PI * userRad * userRad;
         Console.WriteLine("Area of Circle is:" + area);
     }
 
@@ -22,31 +23,35 @@ class Program
     {
          
         Console.WriteLine($"Enter Base Length of Triangle: {baseLength} ");
-        baseLength = Convert.ToDouble(Console.ReadLine());
+         double userBaseLength = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("User Base Length is:" + userBaseLength);
         Console.WriteLine($"Enter Height of Triangle: {height} ");
-        height = Convert.ToDouble(Console.ReadLine());
-        double area = baseLength * height / 2;
+        double userHeight = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("UserHeight is:" + userHeight);
+        double area = userBaseLength * userHeight / 2;
         Console.WriteLine("Area of Triangle is : {0}", area);
         
     }
 
     public static void RectangleArea(double length, double width)
     {
-        Console.WriteLine($"Enter the Length of the Rectangle: {length} ");
-        length = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine($"Enter the Width of Rectangle: {width}");
-        width = Convert.ToDouble(Console.ReadLine());
-        double area = length * width;
+        Console.WriteLine($"Enter the Length of the Rectangle. {length}");
+        double userLength = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("UserLength is:" + userLength);
+        Console.WriteLine($"Enter the Width of Rectangle. {width}");
+        double userWidth = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("UserWidth is :" + userWidth);
+        double area = userLength * userWidth;
         Console.WriteLine($"The area of the Rectangle is: {area}");
     }
 
     public static double SquareArea()
     {
         Console.WriteLine("Enter measurement of side.");
-        double userInput = double.Parse(Console.ReadLine());
-        double side = userInput;
-        double area = side * side;
-        Console.WriteLine($"Area of Square with side {side} is {area} ");
+        double userSide = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("UserSide is:" + userSide);
+        double area = userSide * userSide;
+        Console.WriteLine($"Area of Square with side {userSide} is {area} ");
         Console.ReadKey();
         return area;
 
